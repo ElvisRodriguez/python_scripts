c794a91ccf65b92faa4db889d713e1c7c168c1dd
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2020-02-26 14:15:08 -0500

    Added aliases backup

---
+++ utils/bash_aliases_backup.sh
@@ -0,0 +1,24 @@
+#Custom Alias
+alias rstudio="~/rstudio/bin/rstudio"
+alias python="~/anaconda3/bin/python3.7"
+alias python3="~/anaconda3/bin/python3.7"
+alias studio='cd ~ && .//android-studio/bin/studio.sh'
+alias get-companies='python ~/daily_coding_problems/python/company_name_checker.py'
+alias status='python ~/python_scripts/utils/run_git.py'
+alias yt-dl='python ~/HushProjects/youtubeMP3/yt_converter.py'
+alias sql='sudo mysql -u root -p'
+alias postgres='sudo -u postgres psql'
+alias pymake='python ~/python_scripts/utils/pycreate.py'
+alias pytest='python ~/python_scripts/utils/pytest.py'
+alias count-loc='python ~/python_scripts/utils/lines_of_code.py'
+alias please='sudo'
+alias pip-backup='python ~/python_scripts/utils/pip_package_backup.py backup'
+alias file-backup='dpkg --get-selections | grep -v deinstall > ~/python_scripts/utils/ubuntu-file-backup'
+alias cron-history='vim /var/mail/elvisrodriguez1992'
+alias yellow-prompt=$'export PS1=\'\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;33m\]root@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \''
+alias green-prompt=$'export PS1=\'\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]root@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ \''
+alias extract-tar='tar xzvf'
+alias postman='./Postman/Postman'
+alias docker='sudo docker'
+alias kite='./.local/share/kite/kited'
+alias upgrade-all-pip='python3 python_scripts/utils/upgrade_pip_packages.py'
