a3c553da885168a5ed0f27534deecfdf7966edb9
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-02-19 19:53:27 -0500

    Removed push option in preparation for using ssh to access repositories)

--- utils/run_git.py
+++ utils/run_git.py
@@ -3,6 +3,7 @@ import sys
 
 
 HOME_DIRECTORY = '/home/elvisrodriguez1992'
+LINE_BREAK_SIZE = 100
 
 
 def find_git_directories():
@@ -16,28 +17,15 @@ def find_git_directories():
     return directories
 
 
-def find_git_status(directories, push=False):
+def find_git_status(directories):
     for directory in directories:
         print('In Directory:', directory)
         path = os.path.join(HOME_DIRECTORY, directory)
         os.chdir(path)
         os.system('git status')
-        print('*{symbol}*'.format(symbol='*' * 50))
-        if push:
-            username = sys.argv[2]
-            password = sys.argv[3]
-            os.system('git add .')
-            os.system('git commit -m "git automatically pushed by daemon"')
-            push_command = 'git push https://{username}:{password}@{directory}.biz/file.git --all'
-            push_command = push_command.format(username=username, password=password, directory=directory)
-            os.system(push_command)
+        print('*{symbol}*'.format(symbol='*' * LINE_BREAK_SIZE))
 
 
 if __name__ == '__main__':
     directories = find_git_directories()
-    try:
-        push = sys.argv[1]
-        push = True
-    except IndexError:
-        push = False
-    find_git_status(directories, push)
+    find_git_status(directories)
