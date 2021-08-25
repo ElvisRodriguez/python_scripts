388007470f85d8b18f7d91c0fc593fdf21320f0d
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-08-25 13:58:31 -0400

    Cleaned up functions, used better names, and added some documentation

--- utils/run_git.py
+++ utils/run_git.py
@@ -1,31 +1,41 @@
 import os
-import sys
 
 
 HOME_DIRECTORY = os.getcwd()
-LINE_BREAK_SIZE = 100
 
-
-def find_git_directories():
-    directories = []
+def find_git_repositories():
+    """Crawls through every directory in HOME_DIRECTORY to find git repos
+    Args:
+    - None
+    Returns:
+    - respositories: list[str] absolute paths to every git repo found
+    """
+    repositories = []
     os.chdir(HOME_DIRECTORY)
-    for directory in os.listdir():
-        if os.path.isdir(directory) and '.config' not in directory:
-            directory_path = os.path.join(HOME_DIRECTORY, directory)
-            if '.git' in os.listdir(directory_path):
-                directories.append(directory)
-    return directories
+    for file in os.listdir():
+        if os.path.isdir(file):
+            directory = os.path.join(HOME_DIRECTORY, file)
+            if ".git" in os.listdir(directory):
+                repositories.append(directory)
+    return repositories
 
 
-def find_git_status(directories):
-    for directory in directories:
-        print('In Directory:', directory)
-        path = os.path.join(HOME_DIRECTORY, directory)
+def find_git_status(repositories):
+    """Checks git status of every repo found and pretty prints the result
+    Args:
+    - repositories: list[str] absolute paths to every git repo found
+    Returns:
+    - None
+    """
+    line_break_size = int(os.popen("tput cols").read())
+    for repository in repositories:
+        print("In Repository:", repository)
+        path = os.path.join(HOME_DIRECTORY, repository)
         os.chdir(path)
-        os.system('git status')
-        print('*{symbol}*'.format(symbol='*' * LINE_BREAK_SIZE))
+        os.system("git status")
+        print("{symbol}".format(symbol="*" * line_break_size))
 
 
-if __name__ == '__main__':
-    directories = find_git_directories()
-    find_git_status(directories)
+if __name__ == "__main__":
+    repositories = find_git_repositories()
+    find_git_status(repositories)
