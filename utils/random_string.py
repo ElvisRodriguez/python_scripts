13b494e51fe28ff2fac4643547ba470bd6c3caa7
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-09-21 17:40:12 -0400

    Removed less impactful scripts. Added documentation to other scripts

--- utils/random_string.py
+++ utils/random_string.py
@@ -1,14 +1,26 @@
+"""
+Generate a random string of variable types of characters.
+"""
 import string
 import random
 
 
 def generate_random_string(
-    length, mixed_case=False, include_numbers=False, special_characters=False
+    length, mixed_case=False, include_digits=False, special_characters=False
 ):
+    """Generate a random string of characters.
+    Args:
+        - length(int): Desired length of string.
+        - mixed_case(bool): Optional. Include lowercase and uppercase letters.
+        - include_digits(bool): Optional. Include digits 0 - 9.
+        - special_characters(bool): Optional. Include characters from: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    Returns:
+        random_string(str): A random string of characters.
+    """
     all_characters = [string.ascii_lowercase]
     if mixed_case:
         all_characters.append(string.ascii_uppercase)
-    if include_numbers:
+    if include_digits:
         all_characters.append(string.digits)
     if special_characters:
         all_characters.append(string.punctuation)
